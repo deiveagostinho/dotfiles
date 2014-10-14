67712a676faff280d5dd96e9b13e2c91db081fad
Ju Gonçalves <junia.goncalves@gmail.com>
2014-10-14 16:20:24 +0200

    add cask installs to bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,23 +1,20 @@
 # homebrew
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-# ruby & rvm
-\curl -sSL https://get.rvm.io | bash -s stable  # ruby
-gem install bundler
-
-# homebrew installation
+# update homebrew
 brew update
 brew upgrade
 
+# utils
 brew install coreutils
 brew install moreutils
 brew install findutils
 brew install gnu-sed --default-names
 brew install bash
 brew install bash-completion
-
 brew install wget --enable-iri
 
+# macos x utils
 brew install vim --override-system-vi
 brew install homebrew/dupes/grep
 brew install homebrew/dupes/screen
@@ -25,7 +22,7 @@ brew install homebrew/dupes/screen
 # cask
 brew install caskroom/cask/brew-cask
 
-# add formulae
+# add formulae to homebrew
 brew tap homebrew/science                       # science
 brew tap caskroom/fonts                         # fonts
 brew tap caskroom/versions
@@ -41,12 +38,44 @@ brew install julia                              # julia
 brew install opal                               # ocaml
 brew install r                                  # R
 brew install rust                               # rust
+\curl -sSL https://get.rvm.io | bash -s stable  # ruby
 
 # remove outdated versions from the cellar
 brew cleanup
 
-# use Caskfile to install
-bundle Caskfile
+# browsers
+brew cask install firefox 2> /dev/null
+brew cask install google-chrome 2> /dev/null
+
+# dev apps
+brew cask install imagealpha 2> /dev/null
+brew cask install imageoptim 2> /dev/null
+brew cask install iterm2 2> /dev/null
+brew cask install macvim 2> /dev/null
+brew cask install virtualbox 2> /dev/null
+brew cask install livereload 2> /dev/null
+brew cask install vagrant 2> /dev/null
+
+# apps
+brew cask install caffeine 2> /dev/null
+brew cask install vlc 2> /dev/null
+brew cask install zipeg 2> /dev/null
+brew cask install divvy 2> /dev/null
+brew cask install dashlane 2> /dev/null
+brew cask install transmission 2> /dev/null
+brew cask install vlc 2> /dev/null
+
+# quick look
+brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql webp-quicklook suspicious-package && qlmanage -r
+
+# fonts
+brew cask install font-source-code-pro 2> /dev/null
+
+# langs
+brew cask install haskell-platform 2> /dev/null             # haskell
+brew cask install smlnj 2> /dev/null                        # SML
+brew cask install racket 2> /dev/null                       # racket
+brew cask install elm-platform 2> /dev/null                 # elm
 
 # add neobundle to vim
 curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh | sh
@@ -60,7 +89,6 @@ npm install -g stylus
 npm install -g lessc
 npm install -g fuck-you
 gem install compass
-
 
 # symlink .files
 ln -s .vimrc ~/.vimrc
